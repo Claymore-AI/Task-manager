e9108268a278c7f6fc661af0011c3413b050e94f
Claymore-AI <dimalishchuk2003@gmail.com>
2025-04-15 23:05:20 +0300

    add model and register in admin

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from task_manager.models import Task, Worker, TaskType, Position
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["name", "created_date", "priority", "task_type", "deadline"]
+    filter_horizontal = ["assignees"]
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "position"]
+    search_fields = ["position__name",]
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    ...
+
+
--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,54 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from core import settings
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    class PriorityChoices(models.TextChoices):
+        URGENT = "URGENT", _("Urgent")
+        HIGH = "HIGH", _("High")
+        MIDDLE = "MIDDLE", _("Middle")
+        EAZY = "EAZY", _("EAZY")
+
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=10,
+        choices=PriorityChoices,
+        default=PriorityChoices.MIDDLE
+    )
+
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tasks")
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="workers",
+    )
